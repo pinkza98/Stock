d39930de3e835fed1b6b4b3f7a41b692e38823a1
Atthapol <pinkza88@gmail.com>
2021-08-06 03:24:14 +0700

    ++

--- data-stock/stock_main.php
+++ data-stock/stock_main.php
@@ -1,42 +1,17 @@
 <?php 
     require_once('../database/db.php');
-
     if (isset($_REQUEST['delete_id'])) {
       $bn_id = $_REQUEST['delete_id'];
-
       $select_stmt = $db->prepare("SELECT * FROM branch WHERE bn_id = :bn_id");
       $select_stmt->bindParam(':bn_id', $bn_id);
       $select_stmt->execute();
       $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
-
       // Delete an original record from db
       $delete_stmt = $db->prepare('DELETE FROM branch WHERE bn_id = :bn_id');
       $delete_stmt->bindParam(':bn_id', $bn_id);
       $delete_stmt->execute();
-
         header('Location:set_branch.php');
-    }if (isset($_REQUEST['save'])) {
-      $bn_name = $_REQUEST['txt_branch_name'];
-      
-
-      if (empty($bn_name)) {
-          $errorMsg = "Please enter branch name";
-      } else {
-          try {
-              if (!isset($errorMsg)) {
-                  $insert_stmt = $db->prepare("INSERT INTO branch (bn_name) VALUES (:bn_name)");
-                  $insert_stmt->bindParam(':bn_name', $bn_name);
-
-                  if ($insert_stmt->execute()) {
-                      $insertMsg = "Insert Successfully...";
-                      header("refresh:1;set_branch.php");
-                  }
-              }
-          } catch (PDOException $e) {
-              echo $e->getMessage();
-          }
-      }
-  }
+    }
 ?>
 <link rel="icon" type="image/png" href="../components/images/tooth.png"/>
 <!doctype html>
@@ -45,9 +20,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
-
     <title>Plus dental clinic</title>
             <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>   
@@ -65,55 +38,38 @@
   <body>
     
     <?php include('../components/nav_stock.php'); ?>
-
     <header>
-    
       <div class="display-3 text-xl-center">
         <H2>แสดงข้อมูล</H2>  
       </div>
-
     </header>
     <hr><br>
-
     <?php include('../components/content.php')?>
     <?php 
-         if (isset($errorMsg)) {
+        if (isset($errorMsg)) {
     ?>
         <div class="alert alert-danger mb-2">
             <strong>Wrong! <?php echo $errorMsg; ?></strong>
         </div>
     <?php } ?>
-    
-
-    <?php 
-         if (isset($insertMsg)) {
-    ?>
-        <div class="alert alert-success mb-2">
-            <strong>Success! <?php echo $insertMsg; ?></strong>
-        </div>
-    <?php } ?>
    <div class="container px-4">
-  
-  
-  
-  <br>
-   <table class="table table-dark table-hover text-xl-center" id="stock">
-    <thead>
-      <tr class="table-active text-center">
-        <th scope="col">#</th>
-        <th scope="col">ชื่อสาขา</th>
-        <th scope="col">แก้ไข</th>
-        <th scope="col">ลบ</th>
-      
-      </tr>
+    <br>
+    <table class="table table-dark table-hover text-xl-center" id="stock">
+    <thead class="table-dark">
+        <tr class="table-active">
+            <th scope="col">#</th>
+            <th scope="col">ชื่อสาขา</th>
+            <th scope="col">แก้ไข</th>
+            <th scope="col">ลบ</th>
+        </tr>
     </thead>
-    <tbody>
+    <tbody >
     <?php 
           $select_stmt = $db->prepare("SELECT * FROM branch ORDER BY bn_id DESC");
           $select_stmt->execute();
           while ($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
     ?>
-      <tr  class="table-dark">
+      <tr  class="table-light">
         <td><?php echo $row["bn_id"]; ?></td>
         <td><?php echo $row["bn_name"]; ?></td>
         <td><a href="edit/bn_edit.php?update_id=<?php echo $row["bn_id"]; ?>" class="btn btn-warning">View</a></td>
@@ -123,11 +79,7 @@
     </tbody>
   </table>
    </div>
-
-      
-   
    <?php include('../components/footer.php')?>
-   <script src="../node_modules/data-table/datatable.js"></script>
    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php  require_once('database/db.php');?>
 <link rel="icon" type="image/png" href="components/images/tooth.png"/>
 <!doctype html>
 <html lang="en">
@@ -11,29 +12,64 @@
     <title>Plus dental clinic</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <?php include('components/header.php');?>
-    
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>  
+           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>            
+           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
+   
   </head>
   <body>
     
     <?php include('components/nav.php'); ?>
-
     <header>
-    <div class="text-center">
-        <H2>Plus dental clinic</H2>
-    </div>
+      <div class="display-3 text-xl-center">
+      <H2>Plus dental clinic</H2>
+      </div>
     </header>
     <hr><br>
+    
     
     <?php include('components/content.php')?>
     
    <div class="container px-4 bg-transparent text-dark" >
    <div class="mb-3">
-
+   <div class="container px-4">
+    <br>
+    <table class="table table-dark table-hover text-xl-center" id="stock">
+    <thead class="table-dark">
+        <tr class="table-active">
+            
+            <th scope="col">ชื่อสาขา</th>
+            
+        </tr>
+    </thead>
+    <tbody >
+    <?php 
+          $select_stmt = $db->prepare("SELECT * FROM branch ORDER BY bn_id DESC");
+          $select_stmt->execute();
+          while ($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+      <tr  class="table-light">
+       
+        <td><?php echo $row["bn_name"]; ?></td>
+       
+        <?php } ?>
+      </tr>
+    </tbody>
+  </table>
+   </div>
   </div>
    </div>
 
       
- 
+   <script>
+        $(document).ready( function () {
+            
+        $('#stock').DataTable();
+            } );
+    </script>
    <?php include('components/footer.php')?>
    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   </body>
