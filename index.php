33857b103a866f94e9fc680f314e906dd1bffdf8
Atthapol <pinkza88@gmail.com>
2021-08-28 03:11:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
           },
           dom: 'lBfrtip',
           buttons: [
-            'excel', 'csv', 'pdf', 'copy'
+            'excel', 'copy'
           ],
           "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
   });
