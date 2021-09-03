6c31357d1dd72b2e1956caf3e46766321be255a8
Atthapol <pinkza88@gmail.com>
2021-09-03 17:14:39 +0700

    succeed mutit rowdata

--- test_code/sub_test_3.php
+++ test_code/sub_test_3.php
@@ -1,6 +1,6 @@
     <?php
-    include ('../database/db.php'); 
-?>
+    include_once ('../database/db.php'); 
+    ?>
 <html>
 
 <head>
@@ -9,57 +9,13 @@
     <!-- <==========================================booystrap 5==================================================> -->
     <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <!-- <========================================== jquery ==================================================> -->
-    <script src="../node_modules/jquery/dist/jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- <==========================================data-teble==================================================> -->
 
 </head>
 
 <body>
-    <?php 
-    
-    if (isset($_REQUEST['code_item_check'])) {
 
-        $code_item_check = $_REQUEST['code_item_check'];
-        $bn_id =$_REQUEST['txt_user_bn'];
-    
-        $select_item =$db->query("SELECT code_item,item_id,item_name FROM item WHERE code_item='$code_item_check'");
-        $select_item->execute();
-        $row_item = $select_item->fetch(PDO::FETCH_ASSOC);
-        @@extract($row_item);
-        
-        if($row_item['code_item']!= $code_item_check){
-        $errorMsg = "รหัสบาร์โค้ดนี้ยังไม่มีในระบบ";
-        $item_id = $select_item->fetch();
-        }else{
-        @@$select_stock =$db->prepare("SELECT bn_name,SUM(branch_stock_log.item_quantity) as sum ,item.item_id,code_item,item_name FROM stock 
-        INNER JOIN branch_stock ON stock.stock_id = branch_stock.stock_id
-        INNER JOIN branch_stock_log ON branch_stock.full_stock_id = branch_stock_log.full_stock_id_log
-        INNER JOIN branch ON branch_stock.bn_stock = branch.bn_id
-        INNER JOIN item ON stock.item_id = item.item_id
-        WHERE code_item='$code_item' AND branch_stock.bn_stock ='$bn_id' ");
-        $select_stock->execute();
-        $row_stock = $select_stock->fetch(PDO::FETCH_ASSOC);
-        @@extract($row_stock);
-    
-    }
-        
-
-        echo '<script type="text/javascript">';
-        echo "var item_name_db = '$item_name';";
-        echo "var code_item_db = '$code_item';";
-        echo "var item_quantity_db = '$sum';";
-        echo "var bn_name = '$bn_name';"; // ส่งค่า $data จาก PHP ไปยังตัวแปร data ของ Javascript
-    //     echo "var bn_id = '$bn_id';";
-        echo '</script>';
-    
-    //  }
-    //  if(empty($select_stock)){
-    //     $errorMsg = "รหัสบาร์โค้ด";
-    //  }
-    
-   
-    }
-    ?>
     <div class="container">
         <br />
         <?php 
@@ -81,7 +37,7 @@
         <div class="form-group">
 
             <div class="table-responsive">
-                <form method="post" name="add_name" id="add_name">
+                <form method="post" id="list_item" name="list_item">
                     <table class="table table-bordered">
                         <tr>
                             <td>
@@ -90,7 +46,7 @@
                                         $select_bn->execute();
                                         ?>
 
-                                <select class="form-select" name="txt_user_bn" id="bn_name">
+                                <select class="form-select" name="bn_id" id="bn_id">
                                     <!-- <?php echo$row_session['user_bn']?> -->
                                     <option value="1">สาขาปัจจุบันที่สังกัด
                                     </option>
@@ -101,7 +57,7 @@
                                 </select>
                             </td>
                             <td>
-                                <input class="form-control" type="text" name="code_item_check"
+                                <input class="form-control" type="text" name="code_item"
                                     placeholder="Enter your Code item" class="form-control " id="code_item" autofocus>
                             </td>
                             <td>
@@ -110,13 +66,13 @@
                             </td>
                         </tr>
                     </table>
-
                 </form>
+                <form name="add_name" id="add_name">
                 <div class="responsive p-6">
                     <table class="table table-bordered" id="dynamic_field">
                         <thead class="table-dark ">
-                            <th>ลำดับ</th>
                             <th>รหัสบาร์โค้ด</th>
+                            <th>ชื่อรายการ</th>
                             <th>จำนวน</th>
                             <th>ที่มีจำนวน</th>
                             <th>สาขา</th>
@@ -128,57 +84,67 @@
                     </table>
                     <input type="submit" name="submit" id="submit" class="btn btn-primary" value="Add" />
                 </div>
+                </form>
             </div>
 
         </div>
     </div>
 </body>
-
 </html>
-
-<script>
+<script type="text/javascript">
 $(document).ready(function() {
     var i = 1;
     var qty = 1;
-    
+    // var Data = newFormData();
     $("#code_item").keypress(function(event) {
 
+
+        // var code= document.getElementById("code_item").value;
+        // var bn= document.getElementById("bn_id").value;
+       
+
         if (event.keyCode === 13) {
-            // alert("+++");
-            // $('check').click();
             if(!this.value == ""){ 
-                $("#add").click();
-}
-            
-            event.preventDefault(event);
+                event.preventDefault(event);
+           $.ajax({
+            url:'data-php.php',
+            type:'post',
+            dataType:'json',
+            data:$('#list_item').serialize(event),
+            success:function(result) {
+                
+                var item_id = result.item_id;
+                var item_name = result.item_name;
+                var code_item = result.code_item;
+                var sum_item = result.sum_item;
+                var bn_name = result.bn_name;
+                i++;
+                $('#dynamic_field').append('<tr id="row' + i +
+            '" class="dynamic-added"><td><input type="text" value="' + code_item+
+            '" placeholder="Enter your Name" class="form-control " name="name[]"/disabled></td><td><input type="text" value="' +
+            item_name +
+            '" placeholder="Enter your Name" class="form-control"/disabled></td><td><input type="text" value="' +
+            qty +
+            '" placeholder="Enter your Name" class="form-control "/></td><td><input type="text" value="' +
+            sum_item +
+            '" placeholder="Enter your Name" class="form-control "/disabled></td><td><input type="text" value="' +
+            bn_name +
+            '" placeholder="Enter your Name" class="form-control "/disabled></td><td><button type="button" name="remove" id="' +
+            i + '" class="btn btn-danger btn_remove">X</button></td></tr>');
+
+            $(document).on('click', '.btn_remove', function() {
+            var button_id = $(this).attr("id");
+            $('#row' + button_id + '').remove();
+        });
+               
+               }
+               
+           }
+           );
+        }
         }
     });
-    $('#add').click(function(event) {
-        i++;
-
-        $('#dynamic_field').append('<tr id="row' + i +
-            '" class="dynamic-added"><td><input type="text" value="' + code_item_db+
-            '" placeholder="Enter your Name" class="form-control name_list" name="name[]"/></td><td><input type="text" value="' +
-            item_name_db +
-            '" placeholder="Enter your Name" class="form-control name_list"/></td><td><input type="text" value="' +
-            qty +
-            '" placeholder="Enter your Name" class="form-control name_list"/></td><td><input type="text" value="' +
-            item_quantity_db +
-            '" placeholder="Enter your Name" class="form-control name_list"/></td><td><input type="text" value="' +
-            bn_name +
-            '" placeholder="Enter your Name" class="form-control name_list"/></td><td><button type="button" name="remove" id="' +
-            i + '" class="btn btn-danger btn_remove">X</button></td></tr>');
-            code_item_check = null;
-            return(code_item_check);
-
-    });
-    $(document).on('click', '.btn_remove', function() {
-        var button_id = $(this).attr("id");
-        $('#row' + button_id + '').remove();
-    });
-
-
-
+    
     $('#submit').click(function() {
         $.ajax({
             url: "name.php",
