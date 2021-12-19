47654c1796f1dc818778fbd0d621cf2e2eb56230
Atthapol <pinkza88@gmail.com>
2021-12-19 18:00:21 +0700

    ++

--- response.php
+++ response.php
@@ -1,14 +1,16 @@
 <?php 
-require_once('database/db.php');
-
-class Response {
-
-    public function highlight_item($code_item,$bn,$min,$max){
-        
-
+class MyClass {
+    public $name;
+    public $age;
+    
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
     }
-
+    // function api_get_data_stock_piq(){
+    //     $data_stock_piq = array();
+    //     $sql_stock_piq = "SELECT * FROM stock_piq";
+    //     $result_stock_piq = mysqli_query($conn, $sql_stock_piq);
 }
-
 
 ?>
